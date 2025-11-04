75b01eac4c1122a06aaffe242c32fd8a803c8063
Li <yoitsthischick@gmail.com>
2025-11-04 00:26:02 -0500

    added fontawesome and more content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+
+let openTab = function(event) {
+    console.log('clicked');
+
+    // Get the clicked h3 element
+    let clickedH3 = event.target;
+
+    // Get the next sibling element (the skills-content div)
+    let content = clickedH3.nextElementSibling;
+
+    // Toggle between showing and hiding
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
